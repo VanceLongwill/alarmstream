bdee0eb21c91598c9dc59c0e9e991b15b5c045ee
Vance Longwill <vancelongwill@gmail.com>
2017-08-03 12:46:01 +0200

    Begin rework to airbnb style

--- src/App.js
+++ src/App.js
@@ -1,156 +1,146 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import moment from 'moment';
-import { createNewAlarm } from './Helpers';
-import { AlarmForm } from './AppForm';
-import { AlarmsFeed} from './AppFeed';
-import {AlarmFormOpenButton} from './Components';
-import { exampleAlarms } from './exampleAlarmData';
-import './css/App.css';
-import './css/Main.css';
-
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import moment from "moment";
+import { createNewAlarm } from "./Helpers";
+import { AlarmForm } from "./AppForm";
+import { AlarmFeed } from "./AppFeed";
+import { AlarmFormOpenButton } from "./Components";
+import { exampleAlarms } from "./exampleAlarmData";
+import "./css/App.css";
+import "./css/Main.css";
 
 export class App extends Component {
-
   state = {
     version: 0.1,
     currentTime: moment(),
     alarmFormOpen: false,
-    alarms:  [],
-  }
+    alarms: []
+  };
 
   componentDidMount() {
     this.interval = setInterval(() => {
-      this.setState({ currentTime: moment() })
+      this.setState({ currentTime: moment() });
       this.updateStore();
     }, 1000);
     this.populateAlarms();
- }
+  }
 
- updateStore = () => {
-   //console.log("stored");
-   localStorage.setItem('alarms', this.alarmsToJson(this.state.alarms));
- }
+  updateStore = () => {
+    //console.log("stored");
+    localStorage.setItem("alarms", this.alarmsToJson(this.state.alarms));
+  };
 
- alarmsToJson = (alarms) => {
-   return JSON.stringify(
-     alarms.map(
-       (alarm, index) => {
-         return Object.assign({}, alarm, {
-           time: alarm.time.unix(),
-           dateCreated: alarm.dateCreated.unix()
-         });
-       }
-     )
-   );
- }
+  alarmsToJson = alarms => {
+    return JSON.stringify(
+      alarms.map((alarm, index) => {
+        return Object.assign({}, alarm, {
+          time: alarm.time.unix(),
+          dateCreated: alarm.dateCreated.unix()
+        });
+      })
+    );
+  };
 
   populateAlarms = () => {
-
-   const cachedAlarms = localStorage.getItem('alarms');
-   if (cachedAlarms) {
-     console.log('using cache');
-     let cachedAlarmsData = JSON.parse(cachedAlarms);
-     this.setState(
-       {
-         alarms: this.sortAlarmsByMostRecent( cachedAlarmsData.map(
-           (alarm, index) => {
-             return Object.assign({}, alarm, {
-               time: moment.unix(alarm.time),
-               dateCreated: moment.unix(alarm.dateCreated)
-             });
-           }
-         ))
-       });
-   } else {
-     this.setState(
-       {
-         alarms: this.sortAlarmsByMostRecent( exampleAlarms.map(
-           (alarm, index) => {
-             return Object.assign({}, alarm, {
-               time: moment.unix(alarm.time),
-               dateCreated: moment.unix(alarm.dateCreated)
-             });
-           }
-         ))
-       });
-     localStorage.setItem('alarms', JSON.stringify(exampleAlarms));
-   }
- }
-  sortAlarmsByMostRecent = (alarms) => {
-    return alarms.sort((a, b) => (b.dateCreated - a.dateCreated));
-  }
-  handleFormSubmit = (attrs) => {
+    const cachedAlarms = localStorage.getItem("alarms");
+    if (cachedAlarms) {
+      console.log("using cache");
+      let cachedAlarmsData = JSON.parse(cachedAlarms);
+      this.setState({
+        alarms: this.sortAlarmsByMostRecent(
+          cachedAlarmsData.map((alarm, index) => {
+            return Object.assign({}, alarm, {
+              time: moment.unix(alarm.time),
+              dateCreated: moment.unix(alarm.dateCreated)
+            });
+          })
+        )
+      });
+    } else {
+      this.setState({
+        alarms: this.sortAlarmsByMostRecent(
+          exampleAlarms.map((alarm, index) => {
+            return Object.assign({}, alarm, {
+              time: moment.unix(alarm.time),
+              dateCreated: moment.unix(alarm.dateCreated)
+            });
+          })
+        )
+      });
+      localStorage.setItem("alarms", JSON.stringify(exampleAlarms));
+    }
+  };
+  sortAlarmsByMostRecent = alarms => {
+    return alarms.sort((a, b) => b.dateCreated - a.dateCreated);
+  };
+  handleFormSubmit = attrs => {
     this.createAlarm(attrs);
     this.toggleAlarmFormVisibility();
-  }
+  };
 
-  createAlarm = (attrs) => {
-    let newAlarms = this.state.alarms.concat( createNewAlarm(attrs) ).sort((a, b) => (b.dateCreated - a.dateCreated));
+  createAlarm = attrs => {
+    let newAlarms = this.state.alarms
+      .concat(createNewAlarm(attrs))
+      .sort((a, b) => b.dateCreated - a.dateCreated);
 
     this.setState({
       alarms: newAlarms
     });
 
-    localStorage.setItem('alarms', this.alarmsToJson(newAlarms));
-  }
+    localStorage.setItem("alarms", this.alarmsToJson(newAlarms));
+  };
 
-  handleAlarmDelete = (alarmId) => {
-    let filteredAlarms = this.state.alarms.filter(a => a.id !== alarmId)
+  handleAlarmDelete = alarmId => {
+    let filteredAlarms = this.state.alarms.filter(a => a.id !== alarmId);
     this.setState({
-      alarms: filteredAlarms,
+      alarms: filteredAlarms
     });
 
-   localStorage.setItem('alarms', this.alarmsToJson(filteredAlarms));
-  }
+    localStorage.setItem("alarms", this.alarmsToJson(filteredAlarms));
+  };
 
-  handleAlarmToggle = (alarmId) => {
+  handleAlarmToggle = alarmId => {
     this.toggleAlarm(alarmId);
-  }
+  };
 
-  toggleAlarm = (alarmId) => {
+  toggleAlarm = alarmId => {
     this.setState({
-      alarms: this.state.alarms.map((alarm)=>{
+      alarms: this.state.alarms.map(alarm => {
         if (alarm.id === alarmId) {
           //console.log("activated alarm: " + (!alarm.isActive));
           return Object.assign({}, alarm, {
-            isActive: !alarm.isActive,
+            isActive: !alarm.isActive
           });
         } else {
           return alarm;
         }
-    }),
-  });
-}
-  toggleAlarmFormVisibility = () => this.setState({ alarmFormOpen: !this.state.alarmFormOpen })
+      })
+    });
+  };
+  toggleAlarmFormVisibility = () =>
+    this.setState({ alarmFormOpen: !this.state.alarmFormOpen });
   render() {
     if (this.state.alarmFormOpen) {
-      return (
-        <AlarmForm
-          onSubmit={this.handleFormSubmit}
-        />
-      );
+      return <AlarmForm onSubmit={this.handleFormSubmit} />;
     } else {
       return (
-            <div className="alarm-container">
-              <div id="currentTime">
-                {this.state.currentTime.format("Mo MMMM, HH:mm")}
-              </div>
-              <Container>
-                <AlarmsFeed alarms={this.state.alarms}
-                  onToggleAlarm={this.handleAlarmToggle}
-                  onDeleteAlarm={this.handleAlarmDelete}
-                />
-              </Container>
-              <div id="bottom">
-                <AlarmFormOpenButton
-                  onClick={this.toggleAlarmFormVisibility}
-                />
-              </div>
-            </div>
+        <div className="alarm-container">
+          <div id="currentTime">
+            {this.state.currentTime.format("Mo MMMM, HH:mm")}
+          </div>
+          <Container>
+            <AlarmsFeed
+              alarms={this.state.alarms}
+              onToggleAlarm={this.handleAlarmToggle}
+              onDeleteAlarm={this.handleAlarmDelete}
+            />
+          </Container>
+          <div id="bottom">
+            <AlarmFormOpenButton onClick={this.toggleAlarmFormVisibility} />
+          </div>
+        </div>
       );
     }
-
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Main extends Component {
-  render() {
-    return(
-        <div id='AlarmApp'>
-          <Header />
-          <App />
-        </div>
-    );
-  }
-}
+const Main = () => (
+  <div id='AlarmApp'>
+    <Header />
+    <App />
+  </div>
+);
 
 ReactDOM.render( <Main />,
   document.getElementById('root'));
